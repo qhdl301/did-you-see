add0b6b54e723aca8ccfbeb773fbd965bc04c568
qhdl301@gmail.com <qhdl301@gmail.com>
2021-12-22 19:25:59 +0900

    angular-todolist 신규 생성

---
+++ todo-list/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// Angular moudle
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
