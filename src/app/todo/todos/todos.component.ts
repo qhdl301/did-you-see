888cc9b1478b154f4486b9cd28ffc62f6efbddcb
qhdl301@gmail.com <qhdl301@gmail.com>
2021-12-23 14:52:51 +0900

    codes: inputbox로 목록 보이게 하기

---
+++ src/app/todo/todos/todos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Type } from '@angular/core';
+
+type TodosComponentProps = {
+  id : number,
+  name : string
+}
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+
+export class TodosComponent {  
+  constructor() { }
+
+  title : string = 'TodoList Angular';
+  list : Array<TodosComponentProps> = [];
+
+  addTask(item:string){
+      this.list.push({id : this.list.length, name : item});
+      console.log("item", this.list);
+  }
+
+}
