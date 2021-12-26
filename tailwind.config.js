d83b35a39231d3065204b140fbf0ba0f4c4865a8
qhdl301@gmail.com <qhdl301@gmail.com>
2021-12-27 00:24:09 +0900

    feat(tailwind): tailwind 적용 해보기

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8ffebf',
+      },  
+    },
+  },
+  variants: {
+    extend: {
+      borderWidth: ['hover'],
+    },
+  },
+  plugins: [],
+}
