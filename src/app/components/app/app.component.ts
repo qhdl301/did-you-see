4f4b54edc8089733a50e1c1a3f0276484ead2c6f
Young Soo Kim <qhdl301@gmail.com>
2022-01-02 17:04:46 +0900

    refactor(tailwind): tailwind 변경 작업 및 프로그래스바 컴포넌트 구현

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +6,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isAuth : boolean = false;
 
   constructor() { }
 }
---
+++ src/app/modules/common/common.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
+
+@NgModule({
+    declarations : [
+        ProgressBarComponent
+    ],
+    imports : [
+        CommonModule,
+    ]
+})
+
+export class CustomCommonModule { }
