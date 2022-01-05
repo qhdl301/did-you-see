da1f1fbec9ca59ad9187c9757671a69f4770f9e8
qhdl301@gmail.com <qhdl301@gmail.com>
2022-01-05 17:10:33 +0900

    refactor(dashboard): dailybox office mock data로 이미지 리스트 구현

--- src/app/modules/movie/dashboard/dashboard.component.ts
+++ src/app/modules/movie/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { BoxOfficeListMock }from 'src/app/services/mocks/dailyBoxOfficeList';
+
+export type DailyBoxOfficeProps = {  
+    movieCd: string,
+    imgUrl : string,
+    title : string,
+    type : string[]
+}
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  dailyBoxOfficeList : DailyBoxOfficeProps[] = BoxOfficeListMock;
 }
