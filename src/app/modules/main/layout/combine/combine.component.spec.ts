fe5a3e104cc7f0f879356a0fb346555f3c3ecd63
KimYoungSoo <qhdl301@gmail.com>
2022-01-17 14:43:07 +0900

    Revert "merge ys/feature-10"
    This reverts commit 7bb5930ece1c1b40808e2b565e9bf844f64cc432.

---
+++ src/app/modules/main/layout/combine/combine.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CombineComponent } from './combine.component';
+
+describe('CombineComponent', () => {
+  let component: CombineComponent;
+  let fixture: ComponentFixture<CombineComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CombineComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CombineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
